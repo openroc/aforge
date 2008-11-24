cda0b1f3ed26d0ea3f3901eb47950828d7fd2e69
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-11-24 18:02:20 +0000

    Looks like Sandro is not going to commit anything else ...

--- branches/sandro.franchi/Samples/Vision/MotionDetector.vb/MainForm.vb
+++
@@ -1,6 +0,0 @@
-Public Class MainForm
-
-	Private Sub AboutToolStripMenuItem_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
-		AboutBox.ShowDialog()
-	End Sub
-End Class
